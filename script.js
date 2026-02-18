b8f1ac95fac3494ba62367ab8e08c1ea4d462aec
Kp9460 <parmarks800@gmail.com>
2026-02-18 23:07:24 +0530

    back button add

---
+++ script.js
@@ -0,0 +1,8 @@
+var allElems = document.querySelectorAll(".elem");
+var allFullElems = document.querySelectorAll(".fullElem")
+
+allElems.forEach(function(elem){
+    elem.addEventListener("click",function(){
+        allFullElems[elem.id].style.display = 'block'
+    })
+})
