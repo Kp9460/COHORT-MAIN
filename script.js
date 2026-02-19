bc037e3c2c30b39af2120adc1ca1a45edfa96886
Kp9460 <parmarks800@gmail.com>
2026-02-19 23:53:21 +0530

    task-list

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var allElems = document.querySelectorAll(".elem");
-var allFullElems = document.querySelectorAll(".fullElem")
+function openFeatures() {
+  var allElems = document.querySelectorAll(".elem");
+  var fullElemPage = document.querySelectorAll(".fullElem");
+  var fullElemPageBackBtn = document.querySelectorAll(".fullElem .back");
 
-allElems.forEach(function(elem){
-    elem.addEventListener("click",function(){
-        allFullElems[elem.id].style.display = 'block'
-    })
-})
+  allElems.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      fullElemPage[elem.id].style.display = "block";
+    });
+  });
+
+  fullElemPageBackBtn.forEach(function (back) {
+    back.addEventListener("click", function () {
+      fullElemPage[back.id].style.display = "none";
+    });
+  });
+}
+openFeatures()
