97a5b4b07015f421a4a0052aa7b45ac0bb2ea89c
Kp9460 <parmarks800@gmail.com>
2026-02-20 22:38:20 +0530

    todo-page-commit

--- script.js
+++ script.js
@@ -15,4 +15,58 @@ function openFeatures() {
     });
   });
 }
-openFeatures()
+openFeatures();
+
+var currentTask = []
+
+if(localStorage.getItem('currentTask')){
+  currentTask = JSON.parse(localStorage.getItem('currentTask'))
+}else{
+  console.log('task list is Empty');
+}
+
+function renderTask() {
+  var allTask = document.querySelector(".allTask");
+
+  var sum = "";
+
+  currentTask.forEach(function (elem) {
+    sum =
+      sum +
+      `<div class="task">
+    <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+    <button>Mark as Completed</button>
+    </div>`;
+  });
+
+  allTask.innerHTML = sum;
+}
+renderTask();
+
+let form = document.querySelector(".addTask form");
+let taskInput = document.querySelector(".addTask form #task-input");
+let taskDetailsInput = document.querySelector(".addTask form textarea");
+let taskCheckbox = document.querySelector(".addTask form #check");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  currentTask.push(
+    {
+      task: taskInput.value,
+      details: taskDetailsInput.value,
+      imp: taskCheckbox.checked
+    }
+)
+  localStorage.setItem('currentTask',JSON.stringify(currentTask))
+  
+  taskInput.value = ''
+  taskDetailsInput.value = ''
+  taskCheckbox.checked = false
+
+  renderTask()
+});
+
+
+
+
